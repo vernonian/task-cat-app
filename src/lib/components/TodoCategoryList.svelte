609bd31e5b43cd42c8b8ccf4005ce427c833a124
vernonian <121730895+vernonian@users.noreply.github.com>
2024-05-17 15:47:04 -0700

    Updated category empty list style

--- src/lib/components/TodoCategoryList.svelte
+++ src/lib/components/TodoCategoryList.svelte
@@ -28,29 +28,41 @@
   }
 </script>
 
-<section class="f-col">
-  <h4 class="category-title">{readableCategoryName}</h4>
-  <ul 
-    class="todo-list list-wrap gap-xxs"
-    role="list"
-    aria-labelledby="list-heading"
-  >
-    <!-- For each task in category Tasks, render the task -->
-    {#each categorizedTasks as todo (todo.id)}
-    <li>
-      <!-- Allow the remove and update events to bubble up through this component -->
-      <TodoTask todo={todo} 
-        on:remove
-        on:update
-      />
-    </li>      
-    {:else}
-      <li>😺 No tasks in this kat-egory! 😺</li>
-  {/each}
-  </ul>
+<section class="f-col category">
+  {#if categorizedTasks.length === 0}
+    <span class="f-row gap-s no-items">
+      <h4 class="category-title">{readableCategoryName}</h4><span>|</span><p>No tasks in this kat-egory 😺</p>
+    </span>
+  {:else}
+    <h4 class="category-title">{readableCategoryName}</h4>
+    <ul 
+      class="todo-list list-wrap gap-xxs"
+      role="list"
+      aria-labelledby="list-heading"
+    >
+      <!-- For each task in category Tasks, render the task -->
+      {#each categorizedTasks as todo (todo.id)}
+        <li>
+          <!-- Allow the remove and update events to bubble up through this component -->
+          <TodoTask todo={todo} 
+            on:remove
+            on:update
+          />
+        </li>      
+      {/each}
+    </ul>
+  {/if}
 </section>
 
 <style>
+  .category {
+    border-top: dashed 1px var(--neutral-5);
+    padding-top: var(--m);
+  }
+
+  .no-items {
+    opacity: 0.6;
+  }
   .category-title {
     text-transform: uppercase;
     font-size: var(--fontsize-body-3);
